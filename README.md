df5f06de33a47bdc03e353d7aa8881eeb4f6562e
dandelea <dandelea@alum.us.es>
2016-03-30 23:42:51 +0200

    Commit inicial

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ Aplicación para la tarea 04 de MC (Matemática Computacional) del Máster de In
  * <code>git clone https://github.com/matplotlib/matplotlib.git</code>
  * <code>cd matplotlib</code>
  * <code>python setup.py install</code>
+* Instalar Numberjack y dependencias para Python3
+ * <code>cd ~</code>
+ * <code>wget ftp://xmlsoft.org/libxml2/libxml2-2.7.2.tar.gz</code>
+ * <code>sudo tar xzf libxml2-2.7.2.tar.gz</code>
+ * <code>cd libxml2-2.7.2</code>
+ * <code>./configure</code>
+ * <code>make</code>
+ * <code>sudo make install</code>
+ * <code>cd ~</code>
+ * <code>sudo apt-get install swig</code>
+ * <code>git clone https://github.com/eomahony/Numberjack</code>
+ * <code>cd Numberjack</code>
+ * <code>git fetch</code>
+ * <code>git checkout -b Python3 origin/Python3</code>
+ * <code>sudo python3 setup.py build</code>
+ * <code>sudo python3 setup.py install</code>
---
+++ main.py
@@ -0,0 +1,100 @@
+import argparse
+import json
+import numpy as np
+
+from Numberjack import *
+
+def run(filepath):
+	with open(filepath) as data_file:
+		data = json.load(data_file)
+	total_volume = data['total_volume']
+	objects = data['objects']
+
+	print("Capacidad del contenedor: " +str(total_volume))
+
+	volumes = []	# Vector vi (Volumen de objeto i)
+	customers = []	# Vector ci (Cliente de objeto i)
+	ids = []		# Vector ids (Identificador de objeto i)
+	for obj in objects:
+		ids.append(obj['id'])
+		volumes.append(obj['volume'])
+		customers.append(obj['customer'])
+
+	print("---Solución al problema 1---")
+	'''
+		Restricción: El volumen no puede exceder la capacidad del contenedor.
+			sum (vi * xi) <= V
+		Función objetivo: Máximo volumen
+			F(X): max sum (vi * xi)
+	'''
+	x = VarArray(len(volumes))	# Vector de variables binarias xi (Objeto i entra en contenedor)
+	model = Model(np.sum(np.array(volumes)*np.array(x))<=total_volume)	# Restrictions
+	model.add(Maximise(np.sum(np.array(volumes)*np.array(x))))			# Objective function
+	solver = model.load("Mistral")
+	solver.solve()
+	
+	print_solution(objects, x)
+
+	print("---Solución al problema 2---")
+	'''
+		Restricción: El volumen no puede exceder la capacidad del contenedor.
+			sum (vi * xi) <= V
+		Función objetivo: Máxima cantidad
+			F(X): max sum (xi)
+	'''
+	x = VarArray(len(volumes))	# Vector de variables binarias xi (Objeto i entra en contenedor)
+	model = Model(np.sum(np.array(volumes)*np.array(x))<=total_volume)	# Restrictions
+	model.add(Maximise(np.sum(np.array(x))))										# Objective function
+	solver = model.load("Mistral")
+	solver.solve()
+
+	print_solution(objects, x)
+
+	print("---Solución al problema 3---")
+	'''
+		Restricciones: 
+			- El volumen no puede exceder la capacidad del contenedor.
+				sum (vi * xi) <= V
+			- Si un objeto es incluido, deben incluirse todos los del mismo cliente.
+
+
+		Función objetivo: Máximo volumen
+			F(X): max sum (vi * xi)
+	'''
+	x = VarArray(len(volumes))	# Vector de variables binarias xi (Objeto i entra en contenedor)
+	#arr = all(o == [np.take(x, np.where(customers==c))[0] for c in np.array(customers)][0] for o in [np.take(x, np.where(customers==c))[0] for c in np.array(customers)])
+
+	arr = [np.take(x, np.where(customers==c))[0] for c in np.array(customers)]
+	print(arr)
+	arr2 = all(for o1 in arr)
+
+	model = Model(np.sum(np.array(volumes)*np.array(x))<=total_volume, 
+			) # Restrictions
+	model.add(Maximise(np.sum(np.array(x))))										# Objective function
+	solver = model.load("Mistral")
+	solver.solve()
+	#bool(np.array([bool((x_ind==0).all() or (x_ind==1).all()) for x_ind in [np.take(x,np.where(customers==c)[0]) for c in np.array(customers)]]).all()))
+
+	print_solution(objects, x)
+
+def restriction_customer(customers, x):
+	customers = np.array(customers)
+	result = True
+	for c in customers:
+		ind = np.where(customers==c)[0]
+		x_ind = np.take(x, ind)
+		print(type(result))
+		result &= bool(((x_ind==1).all() or (x_ind==0).all()))
+	return result
+
+def print_solution(objects, x):
+	for i in range(0,len(x)):
+		objects[i]['in_container'] = x[i].__str__()=='1'
+		print(objects[i])
+	return objects
+
+if __name__=='__main__':
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-p", "--path", required=True, help="Path to scenario JSON file")
+	args = vars(ap.parse_args())
+	run(args['path'])
